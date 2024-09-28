966af710299713c3f84b2458ff6fb95bb7f29a1b
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-09-28 07:34:58 +0000

    2nd commit

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const accId=1234;
+let accEmail="amitji@123";
+var accpassword="sonepe";
+accCity="Jhar";
+
+//accId=2; not allowed
+accEmail="hac@gmol";
+accpassword="sompap"
+accCity="Benglor"
+
+console.log(accId);
+console.table([accId,accEmail,accpassword,accCity])
