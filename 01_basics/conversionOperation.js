b889947ff4d8e66dda6756f0ccdb1f18c1a84729
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-09-28 14:31:51 +0000

    commit

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+/*let score = "true";
+console.log(typeof(score));
+
+let valueinNumber = Number(score);
+console.log(typeof(valueinNumber));
+console.log(valueinNumber);
+
+
+"33" ...33
+"33abc"...NaN
+true...1, false...0
+
+let isLoogedIn="amit";
+let booleanInt = Boolean(isLoogedIn);
+console.log(booleanInt);
+*/
+
+/*
+i.. true
+""...false
+"amit"...true
+*/
+let someNum=32
+let stringNum=String(someNum)
+console.log(stringNum)
+console.log(typeof  stringNum)
