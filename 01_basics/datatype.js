e84f1c5e9c1d5270e06d74a7dc30293413d78337
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-09-28 11:08:57 +0000

    data

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -4,4 +4,21 @@
 
  console.log(3+3);
  console.log("Amit");
+
+ let name="amit"
+ let age=13
+ let isLoggedIn=false;
+let state;
+
+ /*number = 2 to power 54
+ string=""
+ boolean= true/false
+ null= standalone value
+ undefined
+symbol
+
+object*/
+
+console.log(typeof"amit"); //string
+console.log(typeof null); //object
  
