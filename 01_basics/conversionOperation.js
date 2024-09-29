44a5ab029d4b86ae8acfbc52f8f84e4f9b02d92c
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-09-29 14:20:34 +0000

    com

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -19,8 +19,44 @@ console.log(booleanInt);
 i.. true
 ""...false
 "amit"...true
-*/
+
 let someNum=32
 let stringNum=String(someNum)
 console.log(stringNum)
 console.log(typeof  stringNum)
+
+
+//********Operations********
+
+let value=3;
+let negValue=-value;
+console.log(negValue);
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);//2 power 3
+console.log(2%3);
+
+let stri="hello"
+let srt2=" amit"
+let str3="str1+str3";
+console.log(str3);
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log((1+2)*4%3);
+*/
+console.log(+true)
+//console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=2+2;
+
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);
