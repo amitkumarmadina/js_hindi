38b908791e434614e0bc9382365897f95e33314a
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-09-29 15:40:11 +0000

    commit

---
+++ 01_basics/datatypes.summery.js
@@ -0,0 +1,30 @@
+//Primitive datatypes
+//  string number boolean null undefined symbol
+
+//Reference (Non primitive )
+
+//array object function
+
+const id=Symbol('123')
+const newid=Symbol('123')
+
+console.log(id===newid)
+
+const bigNumber=12345465342145n;
+
+
+const heros=["shaktimaan","naagraj","doga"];
+
+let myobj={
+    name: "amit",
+    age: 15,
+    class:12
+}
+
+console.log(myobj);
+
+const myfun=function(){
+    console.log("hello")
+}
+
+console.log(myfun);
