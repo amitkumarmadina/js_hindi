eb1ef09007b44fa0e3ee0f6e94edfc2d6c892a7b
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-10-01 12:20:27 +0000

    commit

---
+++ 01_basics/number.js
@@ -0,0 +1,37 @@
+/*const score=400
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(typeof balance);
+console.log(balance.toFixed(3));
+
+
+const othNumber=23.8966;
+console.log(othNumber.toPrecision(15));
+
+
+const hundreds=10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Math)
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+*/
+console.log(Math.random()); //random number between 0 & 1
+const rand=Math.random()*10 +1;
+
+console.log(rand);
+
+console.log(Math.round(rand))
+
+const min=10;
+const max=20;
+
+console.log(Math.random()*(max-min+1)*min)
+
---
+++ 01_basics/string.js
@@ -0,0 +1,34 @@
+const name ="amit"
+const repo=50
+//console.log(name+repo+"value")  Outdated
+//
+console.log(`hello my name is ${name} and i have ${repo} repos`); //string interpolation
+
+const gname=new String("hitesh-hc");
+/*
+console.log(gname)
+console.log(gname.__proto__)
+console.log(gname.length);
+console.log(gname.toLocaleUpperCase());
+console.log(gname.charAt(3))
+console.log(gname.indexOf("d"))
+
+const substring=gname.substring(0,4)
+console.log(substring);
+
+const anstring=gname.slice(-8,4)
+console.log(anstring);
+*/
+
+const newString="   amit    ";
+console.log(newString)
+console.log(newString.trim());
+
+
+const url="https/amit@420kumar.com"
+
+console.log(url.replace('@420','-'))
+
+
+console.log(url.includes('amit'));
+
