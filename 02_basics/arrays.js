5e695fe9039e5a84cb9e6bbba4dca39c619d7a64
AMIT KUMAR MADINA <136113175+amitkumarmadina@users.noreply.github.com>
2024-10-03 05:28:08 +0000

    commit

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -32,3 +32,30 @@ console.log(myn1);
 // console.log(myn2);
 console.log("C",myarr);
 
+const marval_heros=["thor","ironman","spiderman"]
+const dc_heros=["aquaman","wonderwomen","superman"]
+
+// marval_heros.push(dc_heros)
+// console.log(marval_heros[3][2]);
+
+const allheroes=marval_heros.concat(dc_heros)
+console.log(allheroes);
+
+const all_newheros=[...marval_heros,...dc_heros]
+//console.log(all_newheros);
+
+const another_arr=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const realarr=another_arr.flat(Infinity)
+console.log(realarr);
+
+
+console.log(Array.isArray("Amit"))
+console.log(Array.from("Amit"));
+console.log(Array.from({name:"amit"})); //intresting case
+
+
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
